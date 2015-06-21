07ddde94a74c3d651e83c9a0e39a8056b3de3f04
Michael <friendstak@gmail.com>
2015-06-21 04:35:35 +0200

    add mongoDb remote servcices

--- EventsProvider/EventsRepository.cs
+++ EventsProvider/EventsRepository.cs
@@ -18,7 +18,8 @@ namespace EventsProvider
         private const string CollectionName = "event";
 
         private readonly IMongoCollection<FbEvent> fbEvents =
-            new MongoClient("mongodb://172.16.101.218:27017").GetDatabase(DatabaseName).GetCollection<FbEvent>(CollectionName);
+            new MongoClient("mongodb://admin:d41d8cd98f00b204e9800998ecf8427e@ds036648.mongolab.com:36648/facebookevent").GetDatabase(DatabaseName).GetCollection<FbEvent>(CollectionName);
+//            new MongoClient("mongodb://172.16.101.218:27017").GetDatabase(DatabaseName).GetCollection<FbEvent>(CollectionName);
 
         public List<FbEvent> GetByCategory(FbCategory category)
         {
